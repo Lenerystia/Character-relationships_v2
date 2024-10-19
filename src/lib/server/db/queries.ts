efd9b3b4c70355734e23c4de2547e8b65f2ebb1a
Lenerystia <marlenamakosza@gmail.com>
2024-10-19 22:41:06 +0200

    add one more combo query for merging character and relation

--- src/lib/server/db/queries.ts
+++ src/lib/server/db/queries.ts
@@ -1,7 +1,29 @@
 import { db } from "$lib/server/db";
-import { characters } from "$lib/server/db/schema";
+import { characters, relations } from "$lib/server/db/schema";
+import { sql, eq } from "drizzle-orm";
+import { alias } from "drizzle-orm/pg-core";
 
 export async function fetchAllCharacters() {
     const char = await db.select().from(characters);
     return char;
+}
+
+export async function fetchAllRelations() {
+    const rel = await db.select().from(relations);
+    return rel;
+}
+
+export async function fetchCharacterRelatedWithSecondCharacter(){
+    const characters2 = alias(characters, "characters2")
+    const CharacterRelatedWithSecondCharacter = await db.select({
+        'idFirstChar': characters.id,
+        'idSecondChar': characters2.id,
+        'First Character': sql`CONCAT(${characters.firstName}, ' ',${characters.lastName} )`,
+        'Second Character': sql`CONCAT(${characters2.firstName}, ' ',${characters2.lastName} )`,
+        'About relationship': relations.about,
+    })
+    .from(relations)
+    .innerJoin(characters, eq(characters.id, relations.idChar1))
+    .innerJoin(characters2, eq(characters2.id, relations.idChar2));
+   return CharacterRelatedWithSecondCharacter
 }
