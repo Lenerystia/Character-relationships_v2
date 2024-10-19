ed4e5a9fcc640b9ac8b4fda1c99318a164631587
Lenerystia <marlenamakosza@gmail.com>
2024-10-19 22:40:33 +0200

    add main menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,8 @@
 <script lang="ts">
-    import '$lib/scripts/app.css'
-
-    export let data: { result: any[] };
-    let AllCharacters = data.result;
-
+	import { navigateBetweenPages } from "$lib/scripts/navigate";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-
-<h1>Character List</h1>
-<ul>
-  {#each data.result as character}
-    <li>{character.firstName}</li>
-  {/each}
-</ul>
-
-{#if AllCharacters && AllCharacters.length > 0}
-    <div>
-        <table>
-            <thead>
-                <tr>
-                    {#each Object.keys(AllCharacters[0]) as colHead}
-                        <th>{colHead}</th>
-                    {/each}
-                </tr>
-            </thead>
-            <tbody>
-                {#each AllCharacters as row}
-                    <tr>
-                        {#each Object.values(row) as cell}
-                            <td>{cell}</td>
-                        {/each}
-                    </tr>
-                {/each}
-            </tbody>
-        </table>
-    </div>
-{:else}
-    <p>No characters found.</p>
-{/if}
+<h1>Menu</h1>
+    <button name="characters" on:click={navigateBetweenPages}>Characters</button>
+    <button name="relations" on:click={navigateBetweenPages}>Relationships</button>
+    <button name="character_rel" on:click={navigateBetweenPages}>Characters relationships</button>
