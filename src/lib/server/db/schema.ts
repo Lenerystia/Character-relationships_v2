99e8b8aec3bd46213b78f18ce080c2b9ea838b91
Lenerystia <marlenamakosza@gmail.com>
2024-10-19 21:29:12 +0200

    add schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,7 +1,14 @@
-import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text } from "drizzle-orm/pg-core";
 
-export const user = pgTable('user', {
-	id: serial('id').primaryKey(),
-	name: text('name').notNull(),
-	age: integer('age')
+export const characters = pgTable('characters', {
+  id: serial('id').primaryKey(),
+  firstName: text('first_name'),
+  lastName: text('last_name'),
+});
+
+export const relations = pgTable('relations',{
+    id: serial('id').primaryKey(),
+    idChar1: serial('id_char_1').references(()=>characters.id),
+    idChar2: serial('id_char_2').references(()=>characters.id),
+    about: text('about'),
 });
