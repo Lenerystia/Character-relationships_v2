c3b647467dd5ca0fe12056d779040f9417980187
Lenerystia <marlenamakosza@gmail.com>
2024-10-19 22:41:20 +0200

    cleaning trash

--- src/routes/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { fetchAllCharacters } from "$lib/server/db/queries";
-
-export const load = async () => {
-    const result = await fetchAllCharacters();
-    return {
-        result
-    };
-};
