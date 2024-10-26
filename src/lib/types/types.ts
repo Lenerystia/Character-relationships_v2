7b190b47416b242d5ae20b3d6a614c5c04af208a
Lenerystia <marlenamakosza@gmail.com>
2024-10-26 21:06:00 +0200

    ad types

---
+++ src/lib/types/types.ts
@@ -0,0 +1,10 @@
+export type Character = {
+    id: number;
+    firstName: string;
+    lastName: string;
+}
+export type Relation = {
+    idChar1: number;
+    idChar2: number;
+    about: string;
+}
