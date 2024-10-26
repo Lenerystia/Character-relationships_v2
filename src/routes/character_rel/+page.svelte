b25eec54fef2f3efbce2428d9a67354768c7b69c
Lenerystia <marlenamakosza@gmail.com>
2024-10-26 20:59:17 +0200

    Charsrels - throw resposibility for view

--- src/routes/character_rel/+page.svelte
+++ src/routes/character_rel/+page.svelte
@@ -1,37 +1,67 @@
 <script lang="ts">
-    import Navigate from "../../components/Navigate.svelte";
+    import Navigate from "../../lib/components/Navigate.svelte";
     import '$lib/scripts/app.css'
 
-    export let data: { result: any[] };
-    let relationBetweenCharacter = data.result;
+    export let data: { characters: Character[]; relations: Relation[] };
 
+    let characters = data.characters;
+    let relations = data.relations;
+
+    type Character = {
+        id: number;
+        firstName: string;
+        lastName: string;
+    }
+
+    type Relation = {
+        idChar1: number;
+        idChar2: number;
+        about: string;
+    }
+
+    // Helper function to get relations for a specific character
+    function getRelationsForCharacter(characterId: number): Relation[] {
+        return relations.filter((rel: Relation) => rel.idChar1 === characterId);
+    }
+    //TODO: (c => c.id === relation.idChar2 - przenieść do funkcji
 </script>
 
-<title>Main page</title>
+<h1>Relations between character</h1>
 
-<h2>Characters relationships</h2>
-        
-{#if data?.result}
-    <div>
-        <table class="rainbow">
-            <thead>
-                <tr>
-                    {#each Object.keys(relationBetweenCharacter[0]) as colHead}
-                        <th>{colHead}</th>
-                    {/each}
-                </tr>
-            </thead>
-            <tbody>
-                {#each relationBetweenCharacter as row}
+<!-- Render the data in a table -->
+{#if characters && relations}
+    <table>
+        <thead>
+            <tr>
+                <th>Character Name</th>
+                <th>Related Character</th>
+                <th>About Relationship</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each characters as character}
+                {#each getRelationsForCharacter(character.id) as relation}
                     <tr>
-                        {#each Object.values(row) as cell}
-                            <td>{cell}</td>
-                        {/each}
+                        <td>
+                            {#if character.firstName}{character.firstName}{:else}Unknown{/if} 
+                            {#if character.lastName}{character.lastName}{:else}Unknown{/if}
+                        </td>
+                        <td>
+
+                            {#each characters.filter(c => c.id === relation.idChar2) as relatedCharacter}
+                                {#if relatedCharacter.firstName}{relatedCharacter.firstName}{:else}Unknown{/if} 
+                                {#if relatedCharacter.lastName}{relatedCharacter.lastName}{:else}Unknown{/if}
+                            {/each}
+                        </td>
+                        <td>{relation.about}</td>
                     </tr>
                 {/each}
-            </tbody>
-        </table>
-    </div>
+            {/each}
+        </tbody>
+    </table>
+{:else}
+    <p>Loading data...</p>
 {/if}
 
+<br>
 <Navigate />
