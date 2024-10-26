bd112b7377e452fdb238ef96d93808a5e9e2d3ef
Lenerystia <marlenamakosza@gmail.com>
2024-10-26 21:05:18 +0200

    delete useless query

--- src/lib/server/db/queries.ts
+++ src/lib/server/db/queries.ts
@@ -1,12 +1,10 @@
 import { db } from "$lib/server/db";
 import { characters, relations } from "$lib/server/db/schema";
-import { sql, eq } from "drizzle-orm";
-import { alias } from "drizzle-orm/pg-core";
 
 //TODO: Why I repeat myself? - change it to parameter - dla zaoszczędzenia 2 linijek nie warto
 export async function fetchAllCharacters() {
-    const char = await db.select().from(characters);
-    return char;
+    const chars = await db.select().from(characters);
+    return chars;
 }
 
 export async function fetchAllRelations() {
@@ -17,17 +15,3 @@ export async function fetchAllRelations() {
 //TODO: adapter - input ID, output - json z informacjami które potrzebuje w takim formacie w jakim je potrzebuje - osobne metodki, które zwrócą co trzeba
 //TODO: LUB pełen pakiet informacji, niech widok sobie wyciągnie/albo dać wszystko i zostawić widokowi do pokazania co potrzebuje
 //paginacja danych - page, podzielenie na paczki - np. 50 osób na stronę
-export async function fetchCharacterRelatedWithSecondCharacter(){
-    const characters2 = alias(characters, "characters2")
-    const CharacterRelatedWithSecondCharacter = await db.select({
-        'idFirstChar': characters.id,
-        'idSecondChar': characters2.id,
-        'First Character': sql`CONCAT(${characters.firstName}, ' ',${characters.lastName} )`,
-        'Second Character': sql`CONCAT(${characters2.firstName}, ' ',${characters2.lastName} )`,
-        'About relationship': relations.about,
-    })
-    .from(relations)
-    .innerJoin(characters, eq(characters.id, relations.idChar1))
-    .innerJoin(characters2, eq(characters2.id, relations.idChar2));
-   return CharacterRelatedWithSecondCharacter
-}
