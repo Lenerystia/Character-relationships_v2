6b9c4d181a825fa570d49a7f8d0fa1e8a273ab88
Lenerystia <marlenamakosza@gmail.com>
2024-10-27 11:34:45 +0100

    params - add (in old version this works) - to repair

---
+++ src/routes/characters/[charId=integer]/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import '$lib/app.css'
+    import { page } from '$app/stores';
+    import Navigate from '$lib/components/Navigate.svelte';
+
+    export let data
+
+    let infoAboutChosenCharacter = data.result;
+
+    export let pageCharacterId: string;
+    pageCharacterId = $page.params.charId;
+    let character = infoAboutChosenCharacter[0];
+
+</script>
+
+<h1>Page for {pageCharacterId} - {character.firstName} {character.lastName}</h1>
+
+<h2>Fist Name: {character.firstName}</h2>
+<h2>Last Name: {character.lastName}</h2>
+<h2>ID: {character.id}</h2>
+<p>//TODO: 404 - brak takiej postaci (przy próbie wpisania id/uuid)</p>
+<p>//TODO: UUID</p>
+<br>
+<Navigate />
