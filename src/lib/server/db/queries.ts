21bbd6bddec354d48d2f290c51645ea5175f079e
Lenerystia <marlenamakosza@gmail.com>
2024-10-27 13:18:46 +0100

    add query to fetch one character

--- src/lib/server/db/queries.ts
+++ src/lib/server/db/queries.ts
@@ -1,7 +1,7 @@
 import { db } from "$lib/server/db";
 import { characters, relations } from "$lib/server/db/schema";
+import { eq } from "drizzle-orm";
 
-//TODO: Why I repeat myself? - change it to parameter - dla zaoszczędzenia 2 linijek nie warto
 export async function fetchAllCharacters() {
     const chars = await db.select().from(characters);
     return chars;
@@ -11,7 +11,8 @@ export async function fetchAllRelations() {
     const rel = await db.select().from(relations);
     return rel;
 }
-//TODO: get id - give it to view and view ask db for necessary data - which user need  - why view should have connection with db?
-//TODO: adapter - input ID, output - json z informacjami które potrzebuje w takim formacie w jakim je potrzebuje - osobne metodki, które zwrócą co trzeba
-//TODO: LUB pełen pakiet informacji, niech widok sobie wyciągnie/albo dać wszystko i zostawić widokowi do pokazania co potrzebuje
-//paginacja danych - page, podzielenie na paczki - np. 50 osób na stronę
+
+export async function fetchCharacter(compared: number) {
+    const char = await db.select().from(characters).where(eq(characters.id, compared));
+    return char;
+}
