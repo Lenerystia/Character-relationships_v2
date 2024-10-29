cc34b4e32e96ab61626a5f4fa2530f83a4e92c38
Lenerystia <marlenamakosza@gmail.com>
2024-10-29 21:45:59 +0100

    prepare to play with tests

--- src/lib/server/db/queries.ts
+++ src/lib/server/db/queries.ts
@@ -1,6 +1,7 @@
 import { db } from "$lib/server/db";
 import { characters, relations } from "$lib/server/db/schema";
 import { eq } from "drizzle-orm";
+import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
 
 export async function fetchAllCharacters() {
     const chars = await db.select().from(characters);
@@ -12,7 +13,10 @@ export async function fetchAllRelations() {
     return rel;
 }
 
-export async function fetchCharacter(compared: number) {
+export async function fetchCharacterTest(db: PostgresJsDatabase<Record<string, never>>, compared: number) {
     const char = await db.select().from(characters).where(eq(characters.id, compared));
     return char;
+}
+export async function fetchCharacter(compared: number) {
+    return fetchCharacterTest(db,compared);
 }
