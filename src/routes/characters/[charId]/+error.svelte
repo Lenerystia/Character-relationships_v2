a242c20b8b4e68a7a9ebc8c0ec95e6a36d862782
Lenerystia <marlenamakosza@gmail.com>
2024-10-29 21:46:47 +0100

    precision compare

--- src/routes/characters/[charId]/+error.svelte
+++ src/routes/characters/[charId]/+error.svelte
@@ -3,11 +3,11 @@
 	import Navigate from "$lib/components/Navigate.svelte";
 </script>
 
-{#if $page.status == 404}
+{#if $page.status === 404}
     <h1>{$page.status}: {$page.error?.message}</h1>
     <p>We're sorry, but we couldn't find the character you were looking for.</p>
 {/if}
-{#if $page.status == 400}
+{#if $page.status === 400}
     <h1>{$page.status}: {$page.error?.message}</h1>
     <p>You, can't to do this.</p>
 {/if}
