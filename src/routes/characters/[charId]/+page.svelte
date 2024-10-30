bb38671d93bc15eaf860addd0384edb4fe8f9a19
Lenerystia <marlenamakosza@gmail.com>
2024-10-30 23:00:22 +0100

    Add types and rename returns

--- src/routes/characters/[charId]/+page.svelte
+++ src/routes/characters/[charId]/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
-    import { page } from '$app/stores';
-    import '$lib/scripts/app.css'
+	import { page } from '$app/stores';
+	import '$lib/scripts/app.css';
+	import type { Character } from '$lib/types/types';
 
-    let characterId = $page.params.charId;
-    export let data;
-    let character = data.result[0];
+	let characterId = $page.params.charId;
+	export let data: { character: Character[] };
+	let character = data.character[0];
 </script>
 
-    <h1>Test character: {characterId}</h1>
-    <h2>Character: {character.firstName} {character.lastName}</h2>
+<h1>Test character: {characterId}</h1>
+<h2>Character: {character.firstName} {character.lastName}</h2>
--- src/routes/relations/+page.server.ts
+++ src/routes/relations/+page.server.ts
@@ -1,8 +1,12 @@
-import { fetchAllRelations } from "$lib/server/db/queries";
+import { fetchAllRelations } from '$lib/server/db/queries';
+import { error } from '@sveltejs/kit';
 
 export const load = async () => {
-    const result = await fetchAllRelations();
-    return {
-        result
-    };
+	const relations = await fetchAllRelations();
+	if (relations.length === 0) {
+		throw error(404, { message: 'Relations not found' });
+	}
+	return {
+		relations
+	};
 };
