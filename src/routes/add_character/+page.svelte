f4189002b14d3dd168d737c1ee07145ec8e30066
Lenerystia <marlenamakosza@gmail.com>
2024-11-02 15:44:05 +0100

    Add page 'add character'

---
+++ src/routes/add_character/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Navigate from '../../lib/components/Navigate.svelte';
+	import '$lib/scripts/app.css';
+</script>
+
+<main>
+	<div id="left">
+		<h1>Add character</h1>
+		<Navigate />
+	</div>
+</main>
