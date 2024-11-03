96d7df3bdf35ac2ee6ff9e618a177166e1e07a09
Lenerystia <marlenamakosza@gmail.com>
2024-11-03 23:02:46 +0100

    Update Navigate.svelte - add next one page button

--- src/lib/components/Navigate.svelte
+++ src/lib/components/Navigate.svelte
@@ -6,4 +6,5 @@
 <button name="characters" on:click={navigateBetweenPages}>Characters</button>
 <button name="relations" on:click={navigateBetweenPages}>Relationships</button>
 <button name="character_rel" on:click={navigateBetweenPages}>Characters relationships</button>
+<button name="add_character" on:click={navigateBetweenPages}>Add character</button>
 <button name="/" on:click={navigateBetweenPages}>Main menu</button>
