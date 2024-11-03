9f96edac72537e28c198e9b77c94eaaabbb0d38a
Lenerystia <marlenamakosza@gmail.com>
2024-11-03 23:05:31 +0100

    If other errors - generic message

--- src/routes/characters/+error.svelte
+++ src/routes/characters/+error.svelte
@@ -7,6 +7,9 @@
 {#if $page.status === 404}
 	<h1>{$page.status}: {$page.error?.message}</h1>
 	<p>We're sorry, but... it looks like you don't have any characters!</p>
+{:else}
+	<h1>{$page.status}: {$page.error?.message}</h1>
+	<p>Sorry, something goes wrong</p>
 {/if}
 
 <p>Come back!</p>
