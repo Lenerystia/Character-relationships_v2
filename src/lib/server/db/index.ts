89d2bcb2c3fbfe5fd52a65b4cae7cb9ef07b62a6
Lenerystia <marlenamakosza@gmail.com>
2024-11-05 21:33:56 +0100

    Add todo to db connection

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -4,3 +4,4 @@ import { env } from '$env/dynamic/private';
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 const client = postgres(env.DATABASE_URL);
 export const db = drizzle(client);
+//TODO DB conn failed - throw error and error page
