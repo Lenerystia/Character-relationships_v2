a18eb5e758424e17aac6599bee0a8ab5256a2acf
Lenerystia <marlenamakosza@gmail.com>
2024-11-05 21:35:39 +0100

    Update schema.ts

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -2,13 +2,13 @@ import { pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const characters = pgTable('characters', {
   id: serial('id').primaryKey(),
-  firstName: text('first_name'),
-  lastName: text('last_name'),
+  firstName: text('first_name').notNull(),
+  lastName: text('last_name').notNull(),
 });
 
 export const relations = pgTable('relations',{
     id: serial('id').primaryKey(),
     idChar1: serial('id_char_1').references(()=>characters.id),
     idChar2: serial('id_char_2').references(()=>characters.id),
-    about: text('about'),
+    about: text('about').notNull(),
 });
