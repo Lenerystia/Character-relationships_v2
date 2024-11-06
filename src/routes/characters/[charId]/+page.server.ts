46a40ca884797e741273e59e90734c6f4f789143
Lenerystia <marlenamakosza@gmail.com>
2024-11-06 23:43:33 +0100

    Condition merge

--- src/routes/characters/[charId]/+page.server.ts
+++ src/routes/characters/[charId]/+page.server.ts
@@ -1,18 +1,15 @@
 import { fetchCharacter } from '$lib/server/db/queries.js';
 import { error } from '@sveltejs/kit';
+import type { TCharacter } from '$lib/types/types';
+
 
 export async function load({ params }) {
-	// canParseToInt
-	// const paramsCharacterId: string = params.charId;
-	// if (!/^\d+$/.test(paramsCharacterId)) {
-	//     throw error(400, { message: "Character ID must contain only digits 0 to 9." });
-	// }
-	//TODO: Dopytać
-	// w momencie w którym ktoś wpisze tekst dostanie to ^, a potem wpisze 0 i dostanie to poniżej - chyba lepiej zostawić tylko to na dole
-	// bo i tak to pierwsze sprawdza tylko czy jest liczbą, co w sumie sprawdza też to na dole - więc po co dublować?
-	// No i przy okazji wywalono dodatkową zmienną, która nie ułatwia niczego
-	const characterId: number = parseInt(params.charId);
-	if (isNaN(characterId)) {
+	// TODO method canParseToInt
+	const paramsCharacterId: string = params.charId;
+	const characterId: number = parseInt(paramsCharacterId);
+	//TODO To nie ma sensu - bo testować mogę tylko stringa, a isNaN to już musi być sprawdzany numer
+	//Do diabła z tym niech będzie tak
+	if (isNaN(characterId) || !(/^\d+$/.test(paramsCharacterId))) {
 		throw error(400, {
 			message: 'Character ID must be a number from 1 to ' + Number.MAX_SAFE_INTEGER
 		});
@@ -23,7 +20,7 @@ export async function load({ params }) {
 			message: 'Character ID must be a number from 1 to ' + Number.MAX_SAFE_INTEGER
 		});
 	}
-	const character = await fetchCharacter(characterId);
+	const character: TCharacter[] = await fetchCharacter(characterId);
 	if (character.length === 0) {
 		throw error(404, { message: 'Character not found' });
 	}
