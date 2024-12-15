feab03c43af866599e41ec98d000f16012ef4de5
lenerystia <marlenamakosza@gmail.com>
2024-12-15 20:42:00 +0100

    drizzle upgrade config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from 'drizzle-kit';
+import type { Config } from 'drizzle-kit';
+import env from './src/env';
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
+	out: "./dev/drizzle",
 
 	dbCredentials: {
-		url: process.env.DATABASE_URL
+		url: env.DATABASE_URL
+		
 	},
-
+	introspect: {
+		casing: "camel",
+	},
 	verbose: true,
 	strict: true,
 	dialect: 'postgresql'
-});
+}) satisfies Config;
