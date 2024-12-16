acf65d9caf974c1103e5bc67d958fd8d781f3325
lenerystia <marlenamakosza@gmail.com>
2024-12-16 22:43:16 +0100

    extend orm config

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,7 +1,26 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
+// import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import postgres from 'postgres';
-import { env } from '$env/dynamic/private';
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = postgres(env.DATABASE_URL);
-export const db = drizzle(client);
-//TODO DB conn failed - throw error and error page
+// import { env } from '$env/dynamic/private';
+// if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+// const client = postgres(env.DATABASE_URL);
+// export const db = drizzle( client, { casing: 'snake_case' })
+// await migrate(db, {migrationsFolder: "./../../../../drizzle"});
+
+import * as schema from '$lib/server/db/schema/schema';
+import env from "../../../env";
+
+export const connection = postgres(env.DATABASE_URL, {
+  max: (env.DB_MIGRATING || env.DB_SEEDING) ? 1 : undefined,
+  onnotice: env.DB_SEEDING ? () => {} : undefined,
+});
+
+export const db = drizzle(connection, {
+  casing: 'snake_case',
+  schema,
+  logger: true,
+});
+
+export type db = typeof db;
+
+export default db;
