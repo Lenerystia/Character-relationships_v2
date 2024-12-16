88791326447e1168ec30aae89523faad9b7d30cf
lenerystia <marlenamakosza@gmail.com>
2024-12-16 22:45:16 +0100

    Change to more oop

--- src/routes/character_rel/+page.server.ts
+++ src/routes/character_rel/+page.server.ts
@@ -1,19 +1,32 @@
+import { error } from '@sveltejs/kit';
+import { Characters } from '$lib/class/Characters';
+import { Relations } from '$lib/class/Relations';
+import { CharacterFormatter } from '$lib/class/CharacterFormatter';
+import { RelationFormatter } from '$lib/class/RelationFormatter';
 import CharacterModel from '$lib/server/model/CharacterModel';
 import { RelationModel } from '$lib/server/model/RelationModel';
 import type { TCharacter, TRelation } from '$lib/types/types';
-import { error } from '@sveltejs/kit';
 
 export async function load() {
-	const characters: TCharacter[] = await CharacterModel.getListCharacters();
-	const relations: TRelation[] = await RelationModel.getListRelations();
-	if (characters.length === 0) {
-		throw error(404, { message: 'Are you sure you have characters?' });
-	}
-	if (relations.length === 0) {
-		throw error(404, { message: 'Relations not found' });
-	}
-	return {
-		characters,
-		relations
-	};
+  // Fetch data from the database
+  const characters: Characters = await CharacterModel.getCharacters();
+  const relations: Relations = await RelationModel.getRelations();
+
+  // Check if the data is empty and throw an error if so
+  if (characters.charactersArray.length === 0) {
+    throw error(404, { message: 'Are you sure you have characters?' });
+  }
+  if (relations.relationsArray.length === 0) {
+    throw error(404, { message: 'Relations not found' });
+  }
+
+  // Convert the instances of Characters and Relations to plain objects
+  const serializedCharacters: TCharacter[] = CharacterFormatter.toPOJOs(characters.charactersArray);
+  const serializedRelations: TRelation[] = RelationFormatter.toPOJOs(relations.relationsArray);
+
+  // Return the serialized data
+  return {
+    characters: serializedCharacters,
+    relations: serializedRelations
+  };
 }
