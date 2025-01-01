b4d77436cfc856faed9a35b1ac4f717c73ed55cd
lenerystia <marlenamakosza@gmail.com>
2025-01-01 12:38:37 +0100

    Rename from models to Repositories, move formatters to directory

--- src/routes/characters/+page.server.ts
+++ src/routes/characters/+page.server.ts
@@ -1,13 +1,17 @@
-import CharacterModel from '$lib/server/model/CharacterModel';
+import type { Characters } from '$lib/class/Characters';
+import { CharacterFormatter } from '$lib/class/formattters/CharacterFormatter';
+import CharacterRepository from '$lib/server/repositories/CharacterRepository';
 import type { TCharacter } from '$lib/types/types';
 import { error } from '@sveltejs/kit';
 
 export const load = async () => {
-	const characters: TCharacter[] = await CharacterModel.getListCharacters();
-	if (characters.length === 0) {
+	const characters: Characters = await CharacterRepository.getCharacters();
+	if (characters.charactersArray.length === 0) {
 		throw error(404, { message: 'Characters not found' });
 	}
+	
+	const serializedCharacters: TCharacter[] = CharacterFormatter.toPOJOs(characters.charactersArray);
 	return {
-		characters
+		characters: serializedCharacters,
 	};
 };
