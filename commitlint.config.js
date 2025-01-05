7c2b93dd67eeb9c9b4565f0f5cfb32bf24624e1d
lenerystia <marlenamakosza@gmail.com>
2025-01-05 13:37:00 +0100

    fix: fix next problems with commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,3 @@
 export default {
     extends: ['@commitlint/config-conventional'],
-    rules: {
-        // 'body-max-line-length': [2, 'always', 100],
-        // 'footer-max-line-length': [2, 'always', 100],
-        // 'header-max-length': [2, 'always', 100],
-    },
 }
