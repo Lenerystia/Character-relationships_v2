b421bb6a42b2d400dd6dfe68e35f8593909cd378
lenerystia <marlenamakosza@gmail.com>
2025-01-08 23:54:08 +0100

    refactor: replace magic numbers with named constants

--- src/routes/relations/+error.svelte
+++ src/routes/relations/+error.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import '$lib/scripts/app.css';
+	import { StatusCodes } from 'http-status-codes';
 	//TODO: Error - when you haven't got relations between your characters
 	//TODO: Error - when you haven't got characters - so you can't have any relations?
 </script>
 
-{#if $page.status === 404}
+{#if $page.status === StatusCodes.NOT_FOUND}
 	<h1>{$page.status}: {$page.error?.message}</h1>
 	<p>We're sorry, but... it looks like you don't have any relations!</p>
 {/if}
--- src/routes/relations/+page.server.ts
+++ src/routes/relations/+page.server.ts
@@ -3,12 +3,14 @@ import type { TRelation } from '$lib/types/types';
 
 import { RelationFormatter } from '$lib/class/formattters/RelationFormatter';
 import { RelationRepository } from '$lib/server/repositories/RelationRepository';
+import { StatusCodes } from 'http-status-codes';
+import { EMPTY } from '$lib/constants';
 import { error } from '@sveltejs/kit';
 
 export const load = async () => {
 	const relations: Relations = await RelationRepository.getRelations();
-	if (relations.relationsArray.length === 0) {
-		throw error(404, { message: 'Relations not found' });
+	if (relations.relationsArray.length === EMPTY) {
+		throw error(StatusCodes.NOT_FOUND, { message: 'Relations not found' });
 	}
 	const serializedRelations: TRelation[] = RelationFormatter.toPOJOs(relations.relationsArray);
 	return {
