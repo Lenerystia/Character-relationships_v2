02d88a678cd872fbc3050f818bec88551f760152
Lenerystia <marlenamakosza@gmail.com>
2025-02-08 22:24:33 +0100

    chore: test and combine new way

--- src/lib/class/Character.ts
+++ src/lib/class/Character.ts
@@ -1,7 +1,7 @@
 export class Character {
-	constructor(
+	public constructor(
 		public readonly id: number,
 		public readonly firstName: string,
-		public readonly lastName: string
+		public readonly lastName: string,
 	) {}
 }
